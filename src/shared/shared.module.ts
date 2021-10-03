49606cae6eb90e6811f452e2b9869930df9d2579
Jorge Villalobos <jorge.villalobos@davita.com>
2021-10-03 17:14:56 -0600

    feat(settings) GPB-7: Storybook settings and Add Shared Module

---
+++ src/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Stories
+import Button from '../stories/button.component';
+
+const STORIES = [
+  Button
+];
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [...STORIES],
+  exports: [...STORIES]
+})
+export class SharedModule { }

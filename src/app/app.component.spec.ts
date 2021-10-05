b2a88d85795d3edd31253e8e9eb24b98c160d048
Jorge Villalobos <jorge.villalobos@davita.com>
2021-10-04 23:25:37 -0600

    test(settings) test ci fail cypress

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(false).toBeTruthy();
+    expect(app).toBeTruthy();
   });
 
   it(`should have as title 'theGoodPizzaClub'`, () => {

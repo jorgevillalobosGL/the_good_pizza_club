a49e8c162ace460438312af947b43f24e8998a22
Jorge Villalobos <jorge.villalobos@davita.com>
2021-10-11 22:08:19 -0600

    feat(storybook) GPB-19: Create Header Storybook

--- src/stories/header/header.component.ts
+++ src/stories/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'sb-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.scss'],
+  selector: "sb-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.scss"],
 })
 export default class HeaderComponent {
   @Input() user: unknown = null;
@@ -11,4 +11,31 @@ export default class HeaderComponent {
   @Output() onLogin = new EventEmitter<Event>();
   @Output() onLogout = new EventEmitter<Event>();
   @Output() onCreateAccount = new EventEmitter<Event>();
+
+  public menuItems = [
+    {
+      name: "Menu",
+      url: "/",
+    },
+    {
+      name: "About Us",
+      url: "about-us",
+    },
+    {
+      name: "Contact",
+      url: "/contact",
+    },
+    {
+      name: "Login / Sign up",
+      url: "/login",
+    },
+    {
+      name: "Help",
+      url: "/help",
+    },
+    {
+      name: "🛒",
+      url: "/shopping",
+    }
+  ];
 }

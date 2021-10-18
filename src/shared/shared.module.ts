144331122fbab10ab00b502b8d74f6541f049053
jorgevillalobosGL <jorge.villalobos@gorillalogic.com>
2021-10-18 11:21:32 -0600

    feat(storybook) GPB-24: Create Pizza Card Story, add all pizza images (#47)
    Co-authored-by: Jorge Villalobos <jorge.villalobos@davita.com>

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 // Stories
 import Button from '../stories/button/button.component';
+import IconButton from '../stories/icon-button/icon-button.component';
 import Dropdown from '../stories/dropdown/dropdown.component';
 import TextInput from '../stories/text-input/text-input.component';
+import PizzaMenuCardComponent from '../stories/pizza-menu-card/pizza-menu-card.component';
 
 const STORIES = [
   Button,
   Dropdown,
-  TextInput
+  TextInput,
+  IconButton,
+  PizzaMenuCardComponent
 ];
 
 @NgModule({

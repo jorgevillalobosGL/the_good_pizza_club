f33d59791e546786ffcb51897a71c9e9ebd54908
Jorge Villalobos <jorge.villalobos@davita.com>
2021-10-25 22:49:15 -0600

    feat(welcome/login) GPB-33: Add menu header, logo and mobile menu

---
+++ cypress/integration/1-getting-started/welcome-login.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('Welcome / Login Page', () => {
+  beforeEach(() => {
+    cy.visit('localhost:4200')
+  })
+
+  it('display desktop menu and logo', () => {
+    cy.get('.desktop-menu').should('be.visible');
+    cy.get('.mobile-menu').should('not.be.visible');
+    cy.get('.logo').should('be.visible');
+  })
+
+  it('display desktop menu and logo', () => {
+    cy.viewport('iphone-6');
+    cy.get('.desktop-menu').should('not.be.visible');
+    cy.get('.mobile-menu').should('be.visible');
+    cy.get('.logo').should('not.be.visible');
+  })
+})

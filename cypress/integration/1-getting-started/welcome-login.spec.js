b7d7769e2de4a3d2f3d2e02c5a3ccf173d399fc7
jorgevillalobosGL <jorge.villalobos@gorillalogic.com>
2021-10-27 16:05:12 -0600

    GPB-31: Create Footer (#70)
    * feat(footer) GPB-31: Create Footer
    
    * test(footer) GPB-31: Fix Footer Cy Test
    
    Co-authored-by: Jorge Villalobos <jorge.villalobos@davita.com>

--- cypress/integration/1-getting-started/welcome-login.spec.js
+++ cypress/integration/1-getting-started/welcome-login.spec.js
@@ -8,13 +8,13 @@ describe('Welcome / Login Page', () => {
   it('display desktop menu and logo', () => {
     cy.get('.desktop-menu').should('be.visible');
     cy.get('.mobile-menu').should('not.be.visible');
-    cy.get('.logo').should('be.visible');
+    cy.get('.main-nav > .logo').should('be.visible');
   })
 
   it('display desktop menu and logo', () => {
     cy.viewport('iphone-6');
     cy.get('.desktop-menu').should('not.be.visible');
     cy.get('.mobile-menu').should('be.visible');
-    cy.get('.logo').should('not.be.visible');
+    cy.get('.main-nav > .logo').should('not.be.visible');
   })
 })

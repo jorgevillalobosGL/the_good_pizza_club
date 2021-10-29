1188200029147fe26dac2a101e2af2402e33167e
jorgevillalobosGL <jorge.villalobos@gorillalogic.com>
2021-10-28 20:39:43 -0600

    feat(login-page) GPB-35: Create login Page (#73)
    Co-authored-by: Jorge Villalobos <jorge.villalobos@davita.com>

--- src/app/auth/welcome/welcome.component.ts
+++ src/app/auth/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
 })
 export class WelcomeComponent {
 
-  constructor() { }
+  public redirectTo(path: string): void {
+    this.router.navigate([path]);
+  }
+
+  constructor(  private router: Router ) { }
+
 }

2585e0321d2f33fbe3ab462d632ff4718231653d
Jorge Villalobos <jorge.villalobos@davita.com>
2021-10-29 19:53:51 -0600

    ffeat(firebase) GPB-34: prod env settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,9 @@
 export const environment = {
   firebase: {
     projectId: 'the-good-pizza-club',
-    appId: '1:705243269757:web:eac1dea0e1c7a1a690bf14',
     storageBucket: 'the-good-pizza-club.appspot.com',
     locationId: 'us-east1',
-    apiKey: 'AIzaSyCcAC85Z1L19XUe3o3UgR_NfAH5ST88IAE',
     authDomain: 'the-good-pizza-club.firebaseapp.com',
-    messagingSenderId: '705243269757',
   },
   production: true
 };

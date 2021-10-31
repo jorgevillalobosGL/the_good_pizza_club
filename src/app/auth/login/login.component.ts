a85fc966ec067e4552d9e50c8710536ab89e7709
Jorge Villalobos <jorge.villalobos@davita.com>
2021-10-30 23:07:57 -0600

    feat(login) GPB-40: Login Implementation using Firebase

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  public loginForm: FormGroup;
 
-  constructor() { }
+  public login() {
+    const { email, password } = this.loginForm.getRawValue();
+    this.authService.login(email, password).subscribe(
+      ({ user }) => {
+        if (!!user) {
+          this.router.navigate(['/home']);
+        }
+      }
+    );
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) {}
 }

b0a61d0cf36d203fbb2add469df9960d3b2f349d
jorgevillalobosGL <jorge.villalobos@gorillalogic.com>
2021-11-06 22:36:11 -0600

    feat(home) GPB-58: Home Header Restructure (#104)
    Co-authored-by: Jorge Villalobos <jorge.villalobos@davita.com>

---
+++ src/app/home/home-header/home-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-header',
+  templateUrl: './home-header.component.html',
+  styleUrls: ['./home-header.component.scss']
+})
+export class HomeHeaderComponent {
+
+  constructor() { }
+
+}

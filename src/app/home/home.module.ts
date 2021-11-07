84d2f13537689fe6519ef011d7cad4de74cc8732
Jorge Villalobos <jorge.villalobos@davita.com>
2021-11-06 22:28:57 -0600

    feat(home) GPB-58: Home Header Restructure

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRouterModule } from './home.router.module';
+
+// Modules
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { SharedModule } from '@app-shared/shared.module';
+// Components
+import { HomeHeaderComponent } from './home-header/home-header.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
+    CarouselModule,
     HomeRouterModule,
   ],
-  declarations: [HomeComponent]
+  declarations: [
+    HomeComponent,
+    HomeHeaderComponent,
+
+  ]
 })
 export class HomeModule { }

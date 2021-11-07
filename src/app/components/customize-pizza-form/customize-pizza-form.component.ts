17e68159dad55666f02d5438ca7d0983ca8dc1b0
Jorge Villalobos <jorge.villalobos@davita.com>
2021-11-07 16:55:21 -0600

    feat(home) GPB-49: Create Customize Pizza Modal Layout

---
+++ src/app/components/customize-pizza-form/customize-pizza-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customize-pizza-form',
+  templateUrl: './customize-pizza-form.component.html',
+  styleUrls: ['./customize-pizza-form.component.scss']
+})
+export class CustomizePizzaFormComponent {
+
+  constructor() { }
+}

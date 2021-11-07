83241275f8293fc9d9f07dc92d665fe6712c5b38
jorgevillalobosGL <jorge.villalobos@gorillalogic.com>
2021-11-07 00:38:27 -0600

    feat(home) GPB-59: Create "Promos of the Day" Section (#105)
    Co-authored-by: Jorge Villalobos <jorge.villalobos@davita.com>

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,15 +7,16 @@ import Header from './stories/header/header.component';
 import Dropdown from './stories/dropdown/dropdown.component';
 import TextInput from './stories/text-input/text-input.component';
 import IconButton from './stories/icon-button/icon-button.component';
-import PizzaMenuCardComponent from './stories/pizza-menu-card/pizza-menu-card.component';
-
+import PromotionCard from './stories/promotion-card//promotion-card.component';
+import PizzaMenuCard from './stories/pizza-menu-card/pizza-menu-card.component';
 const STORIES = [
   Header,
   Button,
   Dropdown,
+  PromotionCard,
   TextInput,
   IconButton,
-  PizzaMenuCardComponent
+  PizzaMenuCard
 ];
 
 @NgModule({

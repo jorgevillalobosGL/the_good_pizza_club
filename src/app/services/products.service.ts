a4d4f6171704e73c3cbcdc970e37a2ad2c0e998d
Jorge Villalobos <jorge.villalobos@davita.com>
2021-11-13 23:23:16 -0600

    feat(pizzas-list) GPB-56: Consume Pizzas Liist from Firebase

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { PizzaMenuCardContent } from '@app-shared/models/general.model';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,84 +10,9 @@ import { PizzaMenuCardContent } from '@app-shared/models/general.model';
 export class ProductsService {
 
   public getPizzasList(): Observable<PizzaMenuCardContent[]> {
-    return of([{
-      bgColorGradientFrom: '#D9571F',
-      bgColorGradientTo: '#F1C380',
-      title: 'EGG & BASIL',
-      numOfGrams: 540,
-      numOfCalories: 1200,
-      description: 'Our famous egg, basil, capers and black pepper. Don’t knock it ‘til you try it.',
-      price: 14,
-      cardBackgroundUrl: 'assets/images/egg-pizza.png'
-    }, {
-      bgColorGradientFrom: '#8F9F19',
-      bgColorGradientTo: '#D2DA99',
-      title: 'VEGGIE FEST',
-      numOfGrams: 520,
-      numOfCalories: 1000,
-      description: 'Mushrooms, purple basil, capers and our famous secret veggie mix.',
-      price: 12,
-      cardBackgroundUrl: 'assets/images/veggie-pizza.png'
-    },
-    {
-      bgColorGradientFrom: '#D91F1F',
-      bgColorGradientTo: '#F1A980',
-      title: 'SALAMI & TOMATO',
-      numOfGrams: 560,
-      numOfCalories: 1500,
-      description: 'What a perfect combination of salami, tomato wedges and sage. Tastes just like home.',
-      price: 16,
-      cardBackgroundUrl: 'assets/images/tomato-pizza.png'
-    },
-    {
-      bgColorGradientFrom: '#46B88F',
-      bgColorGradientTo: '#D0FD88',
-      title: 'PEPPERONI & EGG',
-      numOfGrams: 540,
-      numOfCalories: 1200,
-      description: 'Our famous egg, basil, capers and black pepper. Don’t knock it ‘til you try it.',
-      price: 14,
-      cardBackgroundUrl: 'assets/images/pepperoni-pizza.png'
-    },
-    {
-      bgColorGradientFrom: '#D96D1F',
-      bgColorGradientTo: '#F1D180',
-      title: 'SALAMI & MUSHROOMS',
-      numOfGrams: 560,
-      numOfCalories: 1500,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      price: 16,
-      cardBackgroundUrl: 'assets/images/mushrooms-pizza.png'
-    },
-    {
-      bgColorGradientFrom: '#1B9F19',
-      bgColorGradientTo: '#A6DA99',
-      title: '‘LIVES & ‘SHROOMS',
-      numOfGrams: 520,
-      numOfCalories: 1000,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet porttitor augue cursus viverra. Urna.',
-      price: 12,
-      cardBackgroundUrl: 'assets/images/lives-pizza.png'
-    },
-    {
-      bgColorGradientFrom: '#D91F1F',
-      bgColorGradientTo: '#F19B80',
-      title: 'ITALIAN-TICAN',
-      numOfGrams: 540,
-      numOfCalories: 1200,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu molestie massa odio nulla diam. At.',
-      price: 14,
-      cardBackgroundUrl: 'assets/images/italian-pizza.png'
-    }, {
-      bgColorGradientFrom: '#199F7F',
-      bgColorGradientTo: '#99DAC3',
-      title: 'SUPREME STYLE',
-      numOfGrams: 520,
-      numOfCalories: 1000,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus libero vivamus nunc blandit.',
-      price: 12,
-      cardBackgroundUrl: 'assets/images/supreme-pizza.png'
-    }]);
+    return this.firestore.collection('pizzas').valueChanges().pipe(
+      switchMap(x => of(x as PizzaMenuCardContent[]))
+    );
   }
 
   public getProductsCatalog(): Observable<any> { // TODO TYPE THIS
@@ -233,6 +160,6 @@ export class ProductsService {
     );
   }
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
 
 }

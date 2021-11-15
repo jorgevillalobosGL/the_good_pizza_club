66e7a3a3935e7656bf20b1b7297cf0f18ebe560b
Jorge Villalobos <jorge.villalobos@davita.com>
2021-11-14 19:50:57 -0600

    build GPB-48 Home / Menu  release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
-import { distinctUntilChanged, filter, map, pluck } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
-import { MenuItem } from './shared/general.model';
-import { AuthService } from './services/auth.service';
-
-import { AuthState } from './auth/store/auth.reducer';
-import { Store } from '@ngrx/store';
-import * as AuthActions from './auth/store/auth.actions';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'

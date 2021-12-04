8de487a414165b33888f06941737cbd9bb09a478
Jorge Villalobos <jorge.villalobos@davita.com>
2021-12-03 19:40:01 -0600

    feat(order-complete) GPB-74: Create Order Complete Section

--- src/app/checkout/checkout.router.module.ts
+++ src/app/checkout/checkout.router.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CheckoutComponent } from './checkout.component';
+import { OrderCompleteComponent } from './order-complete/order-complete.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CheckoutComponent,
   },
+  { path: 'order-complete', component: OrderCompleteComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CheckoutRouterModule {}
+export class CheckoutRouterModule { }
---
+++ src/app/checkout/order-complete/order-complete.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-complete',
+  templateUrl: './order-complete.component.html',
+  styleUrls: ['./order-complete.component.scss']
+})
+export class OrderCompleteComponent {
+
+  constructor() { }
+}
